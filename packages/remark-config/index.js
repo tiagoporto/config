9f26237bcb0660ba83c344d824f13b69a15e67b6
Tiago Porto <tiago@tiagoporto.com>
2024-04-26 21:23:15 -0300

    feat(remark-config): create remark package

---
+++ packages/remark-config/index.js
@@ -0,0 +1,43 @@
+import { unified } from 'unified'
+import remarkEmoji from 'remark-emoji'
+import remarkLintRecommended from 'remark-preset-lint-recommended'
+import remarkLintConsistent from 'remark-preset-lint-consistent'
+import remarkLintMarkdownStyleGuide from 'remark-preset-lint-markdown-style-guide'
+import remarkGFM from 'remark-gfm'
+import remarkValidateLinks from 'remark-validate-links'
+import remarkLintNoDeadUrls from 'remark-lint-no-dead-urls'
+import remarkLintNoUrlTrailingSlash from 'remark-lint-no-url-trailing-slash'
+import remarkPresetPrettier from 'remark-preset-prettier'
+
+import remarkRetext from 'remark-retext'
+import retextEnglish from 'retext-english'
+import retextEquality from 'retext-equality'
+import retextQuotes from 'retext-quotes'
+import retextSyntaxMentions from 'retext-syntax-mentions'
+
+export default {
+  settings: {
+    bullet: '-',
+    listItemIndent: 'mixed',
+    incrementListMarker: false,
+  },
+  plugins: [
+    remarkGFM,
+    remarkEmoji,
+    remarkLintConsistent,
+    remarkLintRecommended,
+    remarkLintMarkdownStyleGuide,
+    remarkValidateLinks,
+    [remarkLintNoDeadUrls, { skipLocalhost: true }],
+    remarkLintNoUrlTrailingSlash,
+    [
+      remarkRetext,
+      unified()
+        .use(retextEnglish)
+        .use(retextEquality)
+        .use(retextQuotes)
+        .use(retextSyntaxMentions),
+    ],
+    remarkPresetPrettier,
+  ],
+}
