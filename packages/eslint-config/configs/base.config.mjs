07bb0d45d9e887cb627be8bdc5f91129bf868a6b
Tiago Porto <tiago@tiagoporto.com>
2025-01-09 22:34:38 -0300

    feat(eslint-config): set tsconfig.json as jsonc

--- packages/eslint-config/configs/base.config.mjs
+++ packages/eslint-config/configs/base.config.mjs
@@ -11,14 +11,14 @@ export default [
   // lint JSON files
   {
     files: ['**/*.json'],
-    ignores: ['package-lock.json', '.vscode/*'],
+    ignores: ['package-lock.json', '.vscode/*', 'tsconfig.json'],
     language: 'json/json',
     ...json.configs.recommended,
   },
 
   // lint JSONC files
   {
-    files: ['**/*.jsonc', '.vscode/**/*.json'],
+    files: ['**/*.jsonc', '.vscode/**/*.json', 'tsconfig.json'],
     language: 'json/jsonc',
     ...json.configs.recommended,
   },
