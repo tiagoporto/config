1fc07be8ed16b2506f9e7850d9c1fdfd9ab4771e
Tiago Porto <tiago@tiagoporto.com>
2025-01-11 23:02:46 -0300

    fix(eslint-config): broken typeScript config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,5 +4,5 @@ import tpConfig from '@tiagoporto/eslint-config'
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { languageOptions: { globals: globals.node } },
-  ...tpConfig.configs.base,
+  ...tpConfig.configs.typeScript,
 ]
--- packages/eslint-config/configs/typescript.config.mjs
+++ packages/eslint-config/configs/typescript.config.mjs
@@ -2,11 +2,15 @@ import base, { baseConfig } from './base.config.mjs'
 import tseslint from 'typescript-eslint'
 
 /** @type {import('eslint').Linter.Config[]} */
-export default [
-  ...base,
-  {
-    files: ['**/*.{ts,tsx}'],
-    ...baseConfig,
-    ...tseslint.configs.recommended,
+export default tseslint.config(base, {
+  files: ['**/*.{ts,tsx,mts,cts}'],
+  ...tseslint.configs.recommended[0],
+  ...tseslint.configs.recommended[1],
+  ...tseslint.configs.recommended[2],
+  rules: {
+    ...baseConfig.rules,
+    ...tseslint.configs.recommended[1].rules,
+    ...tseslint.configs.recommended[2].rules,
   },
-]
+  name: 'typescript/recommended',
+})
