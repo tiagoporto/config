27b2bef2148a0f252a72ebdc98fc9ddf5447d0d6
Tiago Porto <tiago@tiagoporto.com>
2025-01-12 19:28:40 -0300

    feat(remark-config): increase deadOrAlive timeout

--- packages/remark-config/index.mjs
+++ packages/remark-config/index.mjs
@@ -1,19 +1,18 @@
-import { unified } from 'unified'
 import remarkEmoji from 'remark-emoji'
-import remarkLintRecommended from 'remark-preset-lint-recommended'
-import remarkLintConsistent from 'remark-preset-lint-consistent'
-import remarkLintMarkdownStyleGuide from 'remark-preset-lint-markdown-style-guide'
 import remarkGFM from 'remark-gfm'
-import remarkValidateLinks from 'remark-validate-links'
 import remarkLintNoDeadUrls from 'remark-lint-no-dead-urls'
 import remarkLintNoUrlTrailingSlash from 'remark-lint-no-url-trailing-slash'
+import remarkLintConsistent from 'remark-preset-lint-consistent'
+import remarkLintMarkdownStyleGuide from 'remark-preset-lint-markdown-style-guide'
+import remarkLintRecommended from 'remark-preset-lint-recommended'
 import remarkPresetPrettier from 'remark-preset-prettier'
-
 import remarkRetext from 'remark-retext'
+import remarkValidateLinks from 'remark-validate-links'
 import retextEnglish from 'retext-english'
 import retextEquality from 'retext-equality'
 import retextQuotes from 'retext-quotes'
 import retextSyntaxMentions from 'retext-syntax-mentions'
+import { unified } from 'unified'
 
 export default {
   settings: {
@@ -28,7 +27,17 @@ export default {
     remarkLintRecommended,
     remarkLintMarkdownStyleGuide,
     remarkValidateLinks,
-    [remarkLintNoDeadUrls, { skipLocalhost: true }],
+    [
+      remarkLintNoDeadUrls,
+      {
+        skipLocalhost: true,
+        skipOffline: true,
+        deadOrAliveOptions: {
+          timeout: 4000,
+          maxRetries: 2,
+        },
+      },
+    ],
     remarkLintNoUrlTrailingSlash,
     [
       remarkRetext,
