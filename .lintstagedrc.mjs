a5b2f34188ccb63804b774459ad813893d12a6fe
Tiago Porto <tiago@tiagoporto.com>
2025-02-09 20:54:43 -0300

    chore: add no-warn-ignore in eslint on lintstaged files

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,9 +1,12 @@
 export default {
   '*': 'prettier --check --ignore-unknown --write',
-  '*.{md,markdown,mdx}': ['remark --frail', 'eslint --max-warnings 0'],
-  '*.{html,yml,json,jsonc,json5}': 'eslint --max-warnings 0',
+  '*.{md,markdown,mdx}': [
+    'remark --frail',
+    'eslint --max-warnings 0 --no-warn-ignored',
+  ],
+  '*.{html,yml,json,jsonc,json5}': 'eslint --max-warnings 0 --no-warn-ignored',
   '*.{js,mjs,cjs,jsx,ts,mts,cts,tsx}': [
-    'eslint --max-warnings 0',
+    'eslint --max-warnings 0 --no-warn-ignored',
     // unit test
   ],
 }
