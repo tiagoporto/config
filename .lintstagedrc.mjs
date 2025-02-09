23395eb32dadd876620b490b7ce29de652ac2536
Tiago Porto <tiago@tiagoporto.com>
2025-02-09 17:12:01 -0300

    chore: add files in lintstaged

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,4 +1,9 @@
 export default {
   '*': 'prettier --check --ignore-unknown --write',
   '*.{md,markdown,mdx}': ['remark --frail', 'eslint --max-warnings 0'],
+  '*.{html,yml,json,jsonc,json5}': 'eslint --max-warnings 0',
+  '*.{js,mjs,cjs,jsx,ts,mts,cts,tsx}': [
+    'eslint --max-warnings 0',
+    // unit test
+  ],
 }
