ae1faa803af8678745ad29c155b04c9143e7483f
Tiago Porto <tiago@tiagoporto.com>
2025-02-09 22:04:13 -0300

    feat(eslint-config): update yml rules

--- packages/eslint-config/src/yml.mjs
+++ packages/eslint-config/src/yml.mjs
@@ -1,21 +1,34 @@
 import eslintPluginYml from 'eslint-plugin-yml'
 
+const config = {
+  plugins: {
+    ...eslintPluginYml.configs['flat/standard'][0].plugins,
+  },
+  languageOptions: {
+    ...eslintPluginYml.configs['flat/standard'][1].languageOptions,
+  },
+  rules: {
+    ...eslintPluginYml.configs['flat/standard'][1].rules,
+    ...eslintPluginYml.configs['flat/standard'][2].rules,
+    ...eslintPluginYml.configs['flat/prettier'][2].rules,
+    'yml/no-empty-mapping-value': 'off',
+    'yml/file-extension': ['warn', { extension: 'yml', caseSensitive: true }],
+  },
+}
+
 /** @type {import('eslint').Linter.Config[]} */
 export const ymlConfig = [
   // YML and YAML files
   {
     files: ['**/*.{yml,YML}', '**/*.{yaml,YAML}'],
-    plugins: {
-      ...eslintPluginYml.configs['flat/standard'][0].plugins,
-    },
-    languageOptions: {
-      ...eslintPluginYml.configs['flat/standard'][1].languageOptions,
-    },
+    ...config,
+  },
+  {
+    files: ['pnpm-*.{yaml,YAML}'],
+    ...config,
     rules: {
-      ...eslintPluginYml.configs['flat/standard'][1].rules,
-      ...eslintPluginYml.configs['flat/standard'][2].rules,
-      ...eslintPluginYml.configs['flat/prettier'][2].rules,
-      'yml/file-extension': ['warn', { extension: 'yml', caseSensitive: true }],
+      ...config.rules,
+      'yml/file-extension': 'off',
     },
   },
 ]
