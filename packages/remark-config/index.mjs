7be3908d9595e91554da41539cdb3adecfe3f16e
Tiago Porto <tiago@tiagoporto.com>
2025-10-02 13:00:06 -0300

    feat(remark-config): skip npm forbidden urls

--- packages/remark-config/index.mjs
+++ packages/remark-config/index.mjs
@@ -1,3 +1,4 @@
+import fs from 'node:fs'
 import path from 'node:path'
 import process from 'node:process'
 import remarkEmoji from 'remark-emoji'
@@ -20,6 +21,29 @@ import { unified } from 'unified'
 const currentPath = process.cwd()
 const folderName = path.basename(currentPath)
 
+function getPackageJsonFiles(directory) {
+  const files = fs.readdirSync(directory)
+  const packageJsonFiles = []
+  for (const file of files) {
+    const filePath = path.join(directory, file)
+    const stat = fs.statSync(filePath)
+    if (stat.isDirectory() && file !== 'node_modules') {
+      packageJsonFiles.push(...getPackageJsonFiles(filePath))
+    } else if (file === 'package.json') {
+      packageJsonFiles.push(filePath)
+    }
+  }
+  return packageJsonFiles
+}
+
+const pkgPaths = getPackageJsonFiles(currentPath)
+const pkgNames = pkgPaths.map((pkgPath) => {
+  return JSON.parse(fs.readFileSync(path.join(pkgPath), 'utf8')).name
+})
+const ignoreAllNpmUrls = pkgNames.map(
+  (pkg) => `https://www.npmjs.com/package/${pkg}`,
+)
+
 export default {
   settings: {
     bullet: '-',
@@ -38,7 +62,11 @@ export default {
       remarkLintNoDeadUrls,
       {
         // TODO: https://github.com/wooorm/dead-or-alive/issues/3
-        skipUrlPatterns: [`https://tiagoporto.github.io/${folderName}/`],
+        skipUrlPatterns: [
+          `https://tiagoporto.github.io/${folderName}/`,
+          // NOTE: avoid npm 403 errors
+          ...ignoreAllNpmUrls,
+        ],
         skipLocalhost: true,
         skipOffline: true,
         deadOrAliveOptions: {
