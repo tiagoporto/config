e15a215143afc2814ef8cb1afc2c0deb8b93b0d2
Tiago Porto <tiago@tiagoporto.com>
2025-12-07 11:38:00 -0300

    feat(github-files): avoid race condition

--- packages/github-files/save-file.mjs
+++ packages/github-files/save-file.mjs
@@ -12,15 +12,19 @@ export const saveFile = ({
     fs.mkdirSync(githubDestinationFolder)
   }
 
-  if (fs.existsSync(dest)) {
-    fs.unlinkSync(dest)
+  const data = fs.readFileSync(source)
+
+  try {
+    const fd = fs.openSync(dest, fs.constants.O_CREAT | fs.constants.O_EXCL | fs.constants.O_RDWR, 0o600)
+    fs.writeFileSync(fd, data)
+    fs.closeSync(fd)
+
+    console.info(`\u001B[0;32m[${pkg.name}] Created ${fileName}\u001B[0m`)
+  } catch {
+    fs.writeFileSync(dest, data)
 
     console.warn(
-      `\u001B[0;33m[${pkg.name}] Deleted existing ${fileName}\u001B[0m`,
+      `\u001B[0;33m[${pkg.name}] Overwrite existing ${fileName}\u001B[0m`,
     )
   }
-
-  const data = fs.readFileSync(source)
-  fs.writeFileSync(dest, data, { mode: 0o444 })
-  console.info(`\u001B[0;32m[${pkg.name}] Created ${fileName}\u001B[0m`)
 }
